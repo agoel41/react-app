622c5b5a9673dc72531d19add1963b4fe5f7d793
Ankur Goel <ankur.goel@merklecxm.com>
2023-03-30 08:42:59 -0700

    destructuring feature in a component

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,9 +1,18 @@
 
-function ProfileCard(props) {
+//function ProfileCard(props) {
+function ProfileCard({title, handle}) {
+
+    // const title = props.title;
+    // const handle = props.handle;
+
+    // Defining this line is similar to above two lines.
+    // This is called destructuring feature.
+    // const { title, handle } = props;
+
     return (
         <div>
-            <div> Title is {props.title} </div>
-            <div> Handle is {props.handle} </div>
+            <div> Title is {title} </div>
+            <div> Handle is {handle} </div>
         </div>
     );
 }
