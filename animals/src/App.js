b208a349cc22d9d201f22ab1cfd7d92a69567dc0
Ankur Goel <ankur.goel@merklecxm.com>
2023-03-31 09:04:08 -0700

    updated reusable animal components

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,21 +1,36 @@
+import './App.css';
 import {useState} from 'react';
+import AnimalShow from './AnimalShow';
+
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'allegator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 
 function App () {
 
-    // state system.
-    const [count, setCount] = useState(0);
+    // state system
+    const [animals, setAnimals] = useState([]);
 
     // Arrow function is another way to define a function without using function keyword.
     const handleClick = () => {
-        setCount(count + 1);
+        //setAnimals([getRandomAnimal()]);
+        setAnimals([...animals, getRandomAnimal()]);
     };
+
+    // Arrow function. 
+    // map is a built in function in JS. When we call map, we take every element in an array.
+    // and map it to transformation function.
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
 
     return (
         <div>
             <button onClick={handleClick}>Add Animal!</button>
-            <div>
-                Number of Animals: {count}
-            </div>
+            <div>{renderedAnimals}</div>
         </div>
     );    
 }
