4483fadf27944776917284a494914174f48fb37c
Ankur Goel <ankur.goel@merklecxm.com>
2023-04-30 16:42:41 -0700

    books app to demonstrate form handling in React

---
+++ books/src/App.js
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+
+function App() {
+
+    // books piece of state.
+    const [books, setBooks] = useState([]);
+
+    return <div>App</div>;
+}
+
+export default App;
