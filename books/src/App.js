a4d298e7b68288b1dac6317a453090fbcaeae5a3
Ankur Goel <ankur.goel@merklecxm.com>
2023-08-01 13:09:27 -0700

    updated react-app

--- books/src/App.js
+++ books/src/App.js
@@ -1,11 +1,34 @@
 import {useState} from 'react';
+import BookCreate from './components/BookCreate';
 
 function App() {
 
     // books piece of state.
     const [books, setBooks] = useState([]);
 
-    return <div>App</div>;
+    // Event handler
+    const createBook = (title) => {
+
+        /**
+         * Note: When using array don't use push() method as it modifies existing array.
+         * For Ex: don't use books.push({id: 123, title: title})
+         */
+
+        /** 
+            This will copy the books array to the updatedBooks array 
+            and then add the new title to the end of the Array.
+        **/
+        const updatedBooks = [...books, {id: 123, title: title}];
+        setBooks(updatedBooks);
+    };
+
+    // onCreate is the event/prop and createBook is event handler.
+    return (
+        <div>
+            {books.length}
+            <BookCreate onCreate={createBook} />
+        </div>
+    );
 }
 
 export default App;
--- books/src/components/BookCreate.js
+++ books/src/components/BookCreate.js
@@ -1,5 +1,32 @@
-function BookCreate() {
-    return <div>BookCreate</div>;
+import { useState } from 'react';
+
+function BookCreate({ onCreate }) {
+
+    // title piece of state.
+    const [title, setTitle] = useState('');
+
+    // Event handler.
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    // Event handler
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    };
+
+    return (
+        <div className="book-create">
+            <h3> Add a Book</h3>
+            <form onSubmit={handleSubmit} >
+                <label>Title</label>
+                <input className="input" value={title} onChange={handleChange} />
+                <button className="button">Create!</button>
+            </form>
+        </div>
+    );
 }
 
 export default BookCreate;
